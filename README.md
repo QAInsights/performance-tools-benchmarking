edbdff324c58fba79eeadc1a9dc6f21817e00f0e
eldaduzman <eldadu1985@gmail.com>
2022-05-09 23:59:41 +0300

    Results added

--- README.md
+++ README.md
@@ -9,11 +9,6 @@
 | 3 	| Locust-single 	| [link](./locust/README-SINGLE.md)  	|
 | 4 	| Locust-dist   	| [link](./locust/README-DIST.md)  	    |
 
-## Test scenario
-1. Each user sends a get request to [petclinic](https://petclinic.ycrash.io) api every 1 second.
-2. Test follows a stepping pattern
-    1. start from 0
-    2. add 50 users evert 30 seconds
-    3. all the way up to 1000 users
-    4. hold for additional 60 seconds.
+## Article:
+[Read the article here](./ARTICLE.md)  
 
--- k6/scripts.js
+++ k6/scripts.js
@@ -1,6 +1,13 @@
 import { sleep } from "k6"
 import http from "k6/http"
 import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+export function handleSummary(data) {
+  return {
+    "summary.html": htmlReport(data),
+  };
+}
 // init
 
 export let options = {
@@ -10,7 +17,7 @@ export let options = {
       startVUs: 0,
       stages: [
         { duration: '60s', target: 1000 },
-        { duration: '900s', target: 1000 }
+        { duration: '3600s', target: 1000 }
       ],
       gracefulRampDown: '0s',
     },
@@ -22,4 +29,3 @@ export default function () {
   let res = http.get("https://petclinic.ycrash.io/")
   console.log(res.status)
   sleep(randomIntBetween(1, 5))
-}
